c274fcd38a4b18cf59a391cf53ad55cb6bc6ace5
Drew McClellan <d.mcclellan013@gmail.com>
2023-03-12 15:08:40 -0400

    started about page and added scroll bar.

--- javascript/search.js
+++ javascript/search.js
@@ -1,7 +1,5 @@
-function searchSite(query) {
-    // Replace with the URL of your website's search results page
-    var searchUrl = 'https://xxlmandalorian013.github.io/Chef-s-IT-Compendium.github.io/search.html';
-
-    // Redirect to the search results page with the user's query
-    window.location.href = searchUrl + encodeURIComponent(query);
-}
+$("#search").on("submit", function(event) {
+    event.preventDefault();
+    var query = $("#query").val();
+    $("#results").load("/search?q=" + query);
+});
